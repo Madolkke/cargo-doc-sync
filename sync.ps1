c4f3b88c0a3357343f02483c4d63f038318eb306
madolchepalooza <madolchepalooza@qq.com>
2022-01-28 17:49:34 +0800

    2022-49-28-17-49-34

--- sync.ps1
+++ sync.ps1
@@ -1,8 +1,10 @@
 git clone --depth=1 https://github.com/rust-lang/cargo.git
+Set-Location "./cargo"
+git log -n1 --format=format:"%H%n%ai" | Out-File -FilePath "../commit-hash.txt"
+Set-Location ".."
 Remove-Item -Force -r "./doc"
 Move-Item -Force "./cargo/src/doc" "./doc"
 Remove-Item -Force -r "./cargo"
 git add .
 $DATE = Get-Date -Format 'yyyy-m-d-H-m-s'
 git commit -m "$DATE"
-git push origin main
