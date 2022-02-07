060bcbd34e83cafd1c2db079c555220ee35e332f
madolchepalooza <madolchepalooza@qq.com>
2022-02-07 22:54:21 +0800

    2022-54-7-22-54-21

--- sync.ps1
+++ sync.ps1
@@ -4,6 +4,7 @@ git log -n1 --format=format:"%H%n%ai" | Out-File -FilePath "../commit-hash.txt"
 Set-Location ".."
 Remove-Item -Force -r "./doc"
 Move-Item -Force "./cargo/src/doc" "./doc"
+Move-Item -Force "./cargo/crates/mdman" "./mdman"
 Remove-Item -Force -r "./cargo"
 git add .
 $DATE = Get-Date -Format 'yyyy-m-d-H-m-s'
